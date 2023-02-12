6a6d58309ea6824fc67db033985753a866cc1a18
Roman Kholev <RKholev@gmail.com>
2023-02-12 21:15:14 +0300

    Домашнее задание 5

--- les5.py
+++ les5.py
@@ -1,18 +1,18 @@
 # 1. Последовательностью Фибоначчи называется последовательность чисел a0, a1, ..., an, ..., где
 # a0 = 0, a1 = 1, ak = ak-1 + ak-2 (k > 1).
 # Требуется найти N-е число Фибоначчи
-# def fibonacci(num):
-#     if num == 0:
-#         fib = 0
-#     elif num == 1:
-#         fib = 1
-#     else:
-#         fib = fibonacci(num-1) + fibonacci(num-2)
-#     return fib
-#
-# n = int(input("введите количество элементов: "))
-# for i in range(n):
-#     print(fibonacci(i))
+def fibonacci(num):
+    if num == 0:
+        fib = 0
+    elif num == 1:
+        fib = 1
+    else:
+        fib = fibonacci(num-1) + fibonacci(num-2)
+    return fib
+
+n = int(input("введите количество элементов: "))
+for i in range(n):
+    print(fibonacci(i))
 
 # Хакер Василий получил доступ к классному журналу и хочет заменить все свои минимальные оценки на максимальные.
 # Напишите программу, которая заменяет оценки Василия, но наоборот: все максимальные – на минимальные.
